1c697194b525b706a5a77594ee6669ec96b57002
md.ja32 <168980492+web-jamil@users.noreply.github.com>
2024-11-30 04:15:01 +0600

    bulid a tip calculator

--- Basic/two_digit_sum.py
+++ Basic/two_digit_sum.py
@@ -14,8 +14,26 @@
 # except ValueError as e:
 #     print(e)
     
-height = float(input("Please enter your height in meters: "))
-weight = float(input("Please enter your weight in kilograms: "))
-bmi = weight / (height ** 2)  # Use division operator and parentheses for correct precedence
-bmi_as_integer = int(bmi)  # Convert BMI to an integer
-print(f"Your BMI as an integer is: {bmi_as_integer}")
+# height = float(input("Please enter your height in meters: "))
+# weight = float(input("Please enter your weight in kilograms: "))
+# bmi = weight / (height ** 2)  # Use division operator and parentheses for correct precedence
+# bmi_as_integer = int(bmi)  # Convert BMI to an integer
+# print(f"Your BMI as an integer is: {bmi_as_integer}")
+
+# age=int(input('enter your age:'))
+# year_left=90-age
+# days_left_of_year=year_left*365
+# weeks_left_of_year=year_left*52
+# months_left_of_year=year_left*12
+# print(f'You have {days_left_of_year} days, {weeks_left_of_year} weeks, and {months_left_of_year} months left.')
+
+
+print("welcome to the tip calculator")
+total_bill=float(input("What was the total bill? $"))
+percentage=int(input("What percentage would you like to give as a tip? "))
+people=int(input("How many people to split the bill? "))
+tip=total_bill*(percentage/100)
+total_bill_with_tip=total_bill+tip
+bill_per_person=total_bill_with_tip/people
+final_amount=round(bill_per_person,2)
+print(f"Each person should pay: ${final_amount}")
