0ef5666522ad574d1bcd62205f367a80f97e5a6d
md.ja32 <168980492+web-jamil@users.noreply.github.com>
2024-12-05 23:42:57 +0600

    git, github and commands

--- Basic/test.py
+++ Basic/test.py
@@ -32,5 +32,99 @@
 # format_string=lambda first,last:f"Name :{first} {last}"
 # print(format_string("John","Doe"))
 
-formatter=lambda name,age:f"My name is {name} and I am {age} Years old.you live longer than you think {name}"
-print(formatter("Alice",25))
+# formatter=lambda name,age:f"My name is {name} and I am {age} Years old.you live longer than you think {name}"
+# print(formatter("Alice",25))
+
+# grade=90
+# if grade>= 65:
+#   print("F")  
+
+# num1,num2=10,20
+# max=num1 if num1>num2 else num2
+# if(num1<num2):
+#   print("num1 is greater than num2")
+
+
+# def password_Check(password):
+#   if password=="Python@99":
+#     print("Password is correct")
+#   else:
+#     print("Password is incorrect")
+# password_Check("Python@99")
+# password_Check("Python@98")
+  
+# hungry=True
+# x="Feed the bear now!" if hungry else "Do not feed the bear"
+# print(x)
+
+
+# a=3
+# print("A is positive") if a>0 else print("A is negative")
+
+
+# num1,num2=10,20
+# print("num1 is greater than num2") if num1>num2 else print("num2 is greater than num1")
+# number=96
+# if number>0: print("number is positive")
+# else: print("number is negative")
+
+# x=2
+# if x>4:
+#   print("x is greater than 4")
+# else: 
+#   print("x is less than 4")
+
+# enter_age=int(input("Enter your age:"))
+# year_require=0
+# if enter_age>=18:
+#   print("you are old enough to drive the car. to reach yout distination")
+# else:
+#   year_require=18-enter_age
+#   print(f"you are not old enough to drive the car. you need {year_require} years to drive the car")
+
+
+# my_year=int(input("Enter your year of birth:"))
+# your_year=int(input("Enter your year of birth:"))
+# if my_year==your_year:
+#   print("My year is greater than your year")
+# else:
+#   difference=my_year-your_year
+#   print(f"My year is greater than your year by {-difference} years")
+
+# grade=96
+# if grade>=90:
+#   print("A Grade")
+# elif grade>=80:
+#   print("B Grade")
+# elif grade>=70:
+#   print("C Grade")
+# elif grade>=60:
+#   print("D Grade")
+# else:
+#   print("F Grade")
+
+# student_grade=float(input("please enter your Grade :"))
+# if student_grade>=90:
+#   print(f"Your Grade is {student_grade} and you got A Grade")
+# elif student_grade>=80:
+#   print(f"Your Grade is {student_grade} and you got B Grade")
+# elif student_grade>=70:
+#   print(f"Your Grade is {student_grade} and you got C Grade")
+# elif student_grade>=60:
+#   print(f"Your Grade is {student_grade} and you got D Grade")
+# else:
+#   print(f"Your Grade is {student_grade} and you got F Grade")
+
+def user_check(choice):
+  if choice==1:
+    print("You have chosen option 1 and that means you are Admin")
+
+  elif choice==2:
+    print("You have chosen option 2 and that means you are User")
+  elif choice==3:
+    print("You have chosen option 3 and that means you are Guest")
+  else:
+    print("You have chosen invalid option")
+ 
+enter_choice=int(input("Enter your choice:"))
+user_check(enter_choice)
